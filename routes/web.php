658597d9bce8ded990ef75103f4296052f997f09
Ameer mujthaba <sinnert79@gmail.com>
2024-12-03 14:10:17 +0530

    fixed namespace admincontroller folder 'admin'

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\MembershipController;
 use App\Http\Controllers\PurchaseMembershipController;
-use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\admin\AdminController;
 use App\Http\Controllers\admin\ManageTierController;
 use App\Http\Controllers\admin\ManageProductsController;
 use App\Http\Controllers\CartController;
