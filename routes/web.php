3fc0b49371bf15bed003058257159a8f2be4fba0
Ameer mujthaba <sinnert79@gmail.com>
2024-12-03 13:00:45 +0530

    another change to case sensitivity

--- routes/web.php
+++ routes/web.php
@@ -99,7 +99,7 @@ Route::middleware(['auth', CheckUserRole::class . ':admin'])->group(function () 
 
 // Welcome Route for Authenticated Users
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
-    Route::get('/welcome', function () {
+    Route::get('/Welcome', function () {
         return view('welcome');
     })->name('welcome');
 });
