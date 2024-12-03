2813c1ec6a5ce44cb86d8aca095cb92f5f09f26b
Ameer mujthaba <sinnert79@gmail.com>
2024-12-03 13:09:47 +0530

    web welcome changed

--- routes/web.php
+++ routes/web.php
@@ -99,7 +99,7 @@ Route::middleware(['auth', CheckUserRole::class . ':admin'])->group(function () 
 
 // Welcome Route for Authenticated Users
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
-    Route::get('/Welcome', function () {
+    Route::get('/welcome', function () {
         return view('welcome');
     })->name('welcome');
 });
