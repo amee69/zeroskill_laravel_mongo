73d7b0b5e8a876508dd69dc0f8f30492c9a50f77
Ameer mujthaba <sinnert79@gmail.com>
2024-12-07 09:55:47 +0530

    changes to composer.lock and single pridcut view

--- resources/views/shop/single-product.blade.php
+++ resources/views/shop/single-product.blade.php
@@ -3,7 +3,7 @@
         <div class="container mx-auto p-6">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-8 items-start">
                 <!-- Product Images -->
-                <div class="product-images">
+                {{-- <div class="product-images">
                     <!-- Display the first image or a default image if no images exist -->
                     <img src="{{ isset($product->images[0]) ? asset($product->images[0]) : asset('storage/images/Noimage.jpg') }}"
                         alt="{{ $product->product_name }}" class="w-full rounded-lg shadow-md object-cover">
@@ -21,7 +21,33 @@
                                 class="w-20 h-20 object-cover border rounded opacity-50">
                         @endif
                     </div>
+                </div> --}}
+
+                <div x-data="{
+                    images: {{ json_encode(array_map(fn($image) => asset($image), $product->images)) }}, 
+                   
+                    currentIndex: 0,
+                    defaultImage: '{{ asset('storage/images/Noimage.jpg') }}'
+                }" class="product-images">
+
+                    <!-- Main Image Display -->
+                    <div class="border rounded-lg">
+                        <img :src="images.length ? images[currentIndex] : defaultImage" 
+                             alt="{{ $product->product_name }}"
+                             class="rounded-lg shadow-md object-cover h-96 w-auto mx-auto border">
+                    </div>
+                    
+
+                    <!-- Thumbnails -->
+                    <div class="flex space-x-2 mt-4">
+                        <template x-for="(image, index) in images" :key="index">
+                            <img :src="image" @click="currentIndex = index"
+                                class="w-16 h-16 object-cover border rounded cursor-pointer">
+                        </template>
+                    </div>
+
                 </div>
+
 
 
                 <!-- Product Details -->
@@ -55,9 +81,9 @@
                     <!-- Product Description -->
                     <p class="text-xl">{{ $product->description }}</p>
 
-                    
 
-                  
+
+
                 </div>
             </div>
         </div>
