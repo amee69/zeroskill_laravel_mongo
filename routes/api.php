0ec75c1918aff7c386429e84ff245273d69a0155
Ameer mujthaba <sinnert79@gmail.com>
2024-12-28 18:45:38 +0530

    personal access token and api modfication stop, After this probably firebase.

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Product;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+//api stuff below
+
+Route::get('/products', function (Request $request) {
+    // Fetch all products with pagination (10 per page) and sort by creation date in descending order
+    $products = Product::orderBy('created_at', 'desc');
+
+    // Return the products as a JSON response
+    return response()->json($products);
+});
