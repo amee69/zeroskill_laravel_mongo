c108c9c5c5f49e15611d35b0215df99d7e985f6a
Ameer mujthaba <sinnert79@gmail.com>
2025-02-01 15:13:54 +0530

    fixed order confirmation text coolor

--- resources/views/shop/order-confirmation.blade.php
+++ resources/views/shop/order-confirmation.blade.php
@@ -2,8 +2,8 @@
     <x-shop-layout>
     <div class="mx-auto max-w-4xl py-40 text-center">
         <h1 class="text-4xl font-bold text-red-500 mb-8">Order Confirmation</h1>
-        <p class="text-lg text-gray-800">Thank you for your order!</p>
-        <p class="text-lg text-gray-600">We will process your order shortly.</p>
+        <p class="text-lg text-white">Thank you for your order!</p>
+        <p class="text-lg text-white">We will process your order shortly.</p>
         <a href="{{ route('shop') }}" class="mt-8 inline-block bg-red-600 text-white px-6 py-3 rounded-md hover:bg-red-700 transition">
             Continue Shopping
         </a>
